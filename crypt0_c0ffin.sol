af52a602b34f61605b0d1ed186fe21cd012cbe4b
w3dada <wadada@skiff.com>
2023-04-26 19:09:49 +0300

    merged

---
+++ crypt0_c0ffin.sol
@@ -0,0 +1,105 @@
+Crypto Coffin is deployed:
+Mumbai: 0x0a1c9d130f3852ef0e03265027661B47cb878268
+
+// SPDX-License-Identifier: MIT
+//
+// crypt0-c0ffin crypt0-c0ffin crypt0-c0ffin crypt0-c0ffin 
+//         ▐█╩╩███▒▒▒▒▒╢▓╢╢▓╢▒▓▀-░░ └---████╩▀█`
+//         ▐█  ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█▀▀█▀▀█▀▀▀▀═ ▐█`
+//        ▄▌▀,██▄                         ▄██ ▀▄▄
+//        █▌ ███▌                        ▐████ ██
+//      ██- ╔██             ▐█U          " ███ `▐█`
+//      ██ ▐███              █U            ████░▐█
+//     ▄█▀ ▐█▀▀  ,▄▄▄▄▄▄▄▄▄▄▄█▌▄▄▄▄▄▄▄▄▄▓▄ ▀▀██▄▄▀▄,
+//    ▐█╫ ▄██    '▀▀▀▀▀▀▀▀▀▀▀█▌▀▀▀▀▀▀▀▀▀██   ████ ██
+//    ▐█║ ███`    `          █▌     '``"`▀  ─████ ██
+//    ▐▀╢╖█▌                 █▌                ██▄▀▀▌
+//   █▌ ███▌                 ▀                 ███  █▌
+//   █████   ╙        '      █U     "       *   ▐████▌
+//    ▐███   ,   ,,,██      ¬█▌      ▐█,,       ▐███,
+//   ██▒██  m█▌.ƒ'▀▀         ▀         ▀`V ▄██  ▐█▀▐█▌
+//    ▐█  █▌   █▌                         ▄µ "─█▌ ██
+//    ▐█∩m▀▀   █▌  ▄▄▄▄▄▄▄█▄▄ ▄▄▄▄▄██▄▄  ▐█▌   ▀▀ ██
+//    ▐█▒▒█▌ ██,,▐███▌,-;▄███ ███▌▒▒▒████,,██ ,██ ██
+//    ▐█▒▒▀▒%▀▀▀██▀▀▀▀▀░▀▀███ ███▀-"▀▀▀▀██▀▀▀ ▀▀▀ ██
+//      ████████▌`  ███████▌   ▐█████▌ ███████  ▐█`
+//      ╟▄ ╒▄▄▄▄ ▐█▄▄█████*██▄██∞█████▄██▀▄▄▄▄█  ▄
+//      ██ ▐█▀`` '``▐▀```` █████ ```````'ß░░░██ ▐█U
+//      ██ ╒▄▀▀██▄▄▄    ╛ ██████▄      ▄▄▄██▌▄▄ ▐█U
+//      '▀█▌▀  ▀▀████▌    ███████    ▐████`!'▀`██`
+//        █▌ ██" ╠'████.   ╛       ▐███╝╜▒ ███⌐██
+//        ▄⌐ ▄▄    ▄▄█▀  ▄ ,, ,  ,.╘▀█▄    ▄▄▄ ▄▄
+//        ▀██▀█▄   ███░,▐█░██░█▌ ██ ▒██    ██▀█▀▀
+//         ▐█ ██   ███▀▀▀███████▄██▀▀██   ]█▌▐█
+//         ╟╙ ██ ╚ ███████░██ █▌ ██████`ⁿ ,█▌▐█-
+//         ▐█N¬█▌  ▀██▄╢▐█'▀█░▀▌ █▌`▄██  ▐█Ü █
+//         ▐▀  █▌    ▀██▄▄       ▄▄██▌   ╘█U ▀
+//         ▐█  █▌     '▀█████▄█████▀  ,  ]█╖▐█
+//           ██ ██                      ██ ██
+//           ██ ██                      ██ ██
+//           ██ ██████████████████████████ ██
+//           ██,██▄▄▄▄▄▄▌▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄██▄██
+//            ████,,`,,,`,▄,,,,,▐,, ▒,└ ███▌
+//             █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌
+//
+pragma solidity ^0.8.18;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract crypt0_c0ffin is ERC1155, Ownable, ReentrancyGuard {
+    uint256 private _currentCoffinId = 0;
+    uint256 public constant MINT_COST = 0.001 ether;
+
+    mapping(uint256 => string) private _occupant;
+    mapping(uint256 => string) private _epitaph;
+    mapping(uint256 => string) private _metadata;
+
+    event Buried(uint256 tokenId, string occupant, string epitaph, string metadata);
+
+    constructor() ERC1155("") {}
+
+    function setURI(string memory newURI) external onlyOwner {
+        _setURI(newURI);
+    }
+
+    function mint(string memory occupant, string memory epitaph, string memory metadata) external payable nonReentrant {
+        require(msg.value >= MINT_COST, "Insufficient payment for coffin.");
+
+        uint256 tokenId = _getNextTokenId();
+
+        _mint(msg.sender, tokenId, 1, "");
+        _occupant[tokenId] = occupant;
+        _epitaph[tokenId] = epitaph;
+        _metadata[tokenId] = metadata;
+
+        emit Buried(tokenId, occupant, epitaph, metadata);
+        _incrementTokenId();
+    }
+
+    function tokenDetails(uint256 tokenId) external view returns (string memory occupant, string memory epitaph, string memory metadata) {
+        require(_exists(tokenId), "Coffin does not exist");
+        occupant = _occupant[tokenId];
+        epitaph = _epitaph[tokenId];
+        metadata = _metadata[tokenId];
+    }
+
+    function withdraw() external onlyOwner {
+        uint256 balance = address(this).balance;
+        require(balance > 0, "No balance to withdraw.");
+        Address.sendValue(payable(owner()), balance);
+    }
+
+    function _getNextTokenId() private view returns (uint256) {
+        return _currentCoffinId;
+    }
+
+    function _incrementTokenId() private {
+        _currentCoffinId++;
+    }
+
+    function _exists(uint256 tokenId) internal view returns (bool) {
+        return bytes(_occupant[tokenId]).length > 0;
+    }
+}
---
+++ mumbai.js
@@ -0,0 +1,544 @@
+const contractAddress = "0x0a1c9d130f3852ef0e03265027661B47cb878268";
+const abi = [
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "account",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "operator",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "bool",
+				"name": "approved",
+				"type": "bool"
+			}
+		],
+		"name": "ApprovalForAll",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "occupant",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "epitaph",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "metadata",
+				"type": "string"
+			}
+		],
+		"name": "Buried",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "occupant",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "epitaph",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "metadata",
+				"type": "string"
+			}
+		],
+		"name": "mint",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "previousOwner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "OwnershipTransferred",
+		"type": "event"
+	},
+	{
+		"inputs": [],
+		"name": "renounceOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256[]",
+				"name": "ids",
+				"type": "uint256[]"
+			},
+			{
+				"internalType": "uint256[]",
+				"name": "amounts",
+				"type": "uint256[]"
+			},
+			{
+				"internalType": "bytes",
+				"name": "data",
+				"type": "bytes"
+			}
+		],
+		"name": "safeBatchTransferFrom",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "id",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			},
+			{
+				"internalType": "bytes",
+				"name": "data",
+				"type": "bytes"
+			}
+		],
+		"name": "safeTransferFrom",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "operator",
+				"type": "address"
+			},
+			{
+				"internalType": "bool",
+				"name": "approved",
+				"type": "bool"
+			}
+		],
+		"name": "setApprovalForAll",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "newURI",
+				"type": "string"
+			}
+		],
+		"name": "setURI",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "operator",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256[]",
+				"name": "ids",
+				"type": "uint256[]"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256[]",
+				"name": "values",
+				"type": "uint256[]"
+			}
+		],
+		"name": "TransferBatch",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "transferOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "operator",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "id",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "TransferSingle",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "value",
+				"type": "string"
+			},
+			{
+				"indexed": true,
+				"internalType": "uint256",
+				"name": "id",
+				"type": "uint256"
+			}
+		],
+		"name": "URI",
+		"type": "event"
+	},
+	{
+		"inputs": [],
+		"name": "withdraw",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "account",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "id",
+				"type": "uint256"
+			}
+		],
+		"name": "balanceOf",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address[]",
+				"name": "accounts",
+				"type": "address[]"
+			},
+			{
+				"internalType": "uint256[]",
+				"name": "ids",
+				"type": "uint256[]"
+			}
+		],
+		"name": "balanceOfBatch",
+		"outputs": [
+			{
+				"internalType": "uint256[]",
+				"name": "",
+				"type": "uint256[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "account",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "operator",
+				"type": "address"
+			}
+		],
+		"name": "isApprovedForAll",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "MINT_COST",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "bytes4",
+				"name": "interfaceId",
+				"type": "bytes4"
+			}
+		],
+		"name": "supportsInterface",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "tokenDetails",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "occupant",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "epitaph",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "metadata",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "uri",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+
+async function loadWeb3() {
+    if (window.ethereum) {
+        window.web3 = new Web3(window.ethereum);
+        await window.ethereum.request({ method: 'eth_requestAccounts' });
+    } else {
+        alert("Please install MetaMask to use this site.");
+    }
+}
+
+async function loadBlockchainData() {
+    const web3 = window.web3;
+    const accounts = await web3.eth.getAccounts();
+    window.account = accounts[0];
+
+    window.contract = new web3.eth.Contract(abi, contractAddress);
+}
+
+async function mintNFT(occupant, epitaph, metadata) {
+    try {
+        const MINT_COST = window.web3.utils.toWei("0.01", "ether"); // Set the minting cost in ether (replace 0.01 with the actual cost)
+
+        await window.contract.methods.mint(occupant, epitaph, metadata)
+            .send({ from: window.account, value: MINT_COST });
+
+        setStatus("Successfully minted NFT!");
+    } catch (error) {
+        console.error(error);
+        setStatus("Error minting NFT.");
+    }
+}
+
+function setStatus(message) {
+    const status = document.getElementById("status");
+    status.innerHTML = message;
+}
+
+mintNFTButton.addEventListener("click", async () => {
+    const occupant = document.getElementById("occupant").value;
+    const epitaph = document.getElementById("epitaph").value;
+    const metadata = document.getElementById("metadata").value;
+
+    if (occupant && epitaph && metadata) {
+        setStatus("Minting NFT...");
+        await mintNFT(occupant, epitaph, metadata);
+    } else {
+        setStatus("Please enter the occupant, epitaph, and metadata.");
+    }
+});
+
+async function initApp() {
+    await loadWeb3();
+    await loadBlockchainData();
+}
+
+initApp();
